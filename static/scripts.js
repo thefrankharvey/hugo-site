42884ef88960ad5bdf1ec7f1bdc1840aa4a6dd1a
frank mcmanus <frank@thefrankharvey.com>
2020-02-10 05:11:54 -0500

    complete dev

--- static/scripts.js
+++ static/scripts.js
@@ -1,6 +1,19 @@
-import "bootstrap";
-import "popper.js";
-import "jquery";
+function handleNav() {
+    const width = window.innerWidth;
+    if (width > 768) {
+      expandNav();
+    }
+  }
 
+  function expandNav() {
+    document.getElementById("sidenav-js").style.width = "140px";
+    document.getElementById("main").style.marginLeft = "140px";
+  }
 
+  function closeNav() {
+    const main = document.getElementById("main");
+    document.getElementById("sidenav-js").style.width = "0";
+    main.style.marginLeft = "0";
+    main.style.width = "width: calc(100% - 90px);";
+  }
   
